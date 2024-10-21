15b72bdd1b03ad5bfaec464a508ed7aec247e051
LuisGMReis <luis.gabrielreis@gmail.com>
2024-10-21 14:49:46 -0300

    Alterações

--- Api.py
+++ Api.py
@@ -3,20 +3,18 @@ from tkinter import ttk
 from tkinter import messagebox
 import mysql.connector
 
-# Função para conectar ao banco de dados
 def conectar_banco():
     try:
         return mysql.connector.connect(
             host="localhost",
             user="root",
-            password="12345", 
-            database="bancopy"  
+            password="12345",
+            database="bancopy"
         )
     except mysql.connector.Error as err:
         messagebox.showerror("Erro", f"Erro ao conectar ao banco de dados: {str(err)}")
         return None
 
-# Função de login para verificar o tipo de usuário
 def login():
     username = entry_username.get().strip()
     senha = entry_senha.get().strip()
@@ -31,8 +29,6 @@ def login():
 
     try:
         cursor = conexao.cursor()
-
-        # Consultar usuário no banco de dados
         cursor.execute("SELECT role FROM tb_usuarios WHERE username=%s AND senha=%s", (username, senha))
         result = cursor.fetchone()
 
@@ -40,7 +36,7 @@ def login():
             role = result[0]
             messagebox.showinfo("Sucesso", f"Login bem-sucedido como {username}!")
             root.destroy()
-            open_main_window(username, role)  # Abre a janela principal com base no papel do usuário
+            open_main_window(username, role)
         else:
             messagebox.showwarning("Aviso", "Usuário ou senha incorretos.")
     except Exception as e:
@@ -49,7 +45,6 @@ def login():
         cursor.close()
         conexao.close()
 
-# Função para abrir a janela principal de acordo com o tipo de usuário
 def open_main_window(username, role):
     main_window = tk.Tk()
     main_window.title("Gerenciador de Resíduos")
@@ -66,21 +61,16 @@ def open_main_window(username, role):
             return
 
         quantidade = int(quantidade)
-
         conexao = conectar_banco()
         if conexao is None:
             return
 
         try:
             cursor = conexao.cursor()
-
-            # Inserir os resíduos no banco de dados, relacionado ao usuário logado
             sql = "INSERT INTO tb_residuos (residuos, estado, quantidade, usuario_id) VALUES (%s, %s, %s, (SELECT id FROM tb_usuarios WHERE username = %s))"
             valores = (residuos, estado, quantidade, username)
             cursor.execute(sql, valores)
-
             conexao.commit()
-
             messagebox.showinfo("Sucesso", "Dados inseridos com sucesso!")
         except Exception as e:
             messagebox.showerror("Erro", f"Erro ao inserir dados: {str(e)}")
@@ -98,7 +88,7 @@ def open_main_window(username, role):
             return
 
         if not residuos:
-            messagebox.showwarning("Aviso", "Digite o código do resíduo a ser removido.")
+            messagebox.showwarning("Aviso", "Digite o nome do resíduo a ser removido.")
             return
 
         conexao = conectar_banco()
@@ -166,15 +156,15 @@ def open_main_window(username, role):
 
     style = ttk.Style()
     style.theme_use("clam")
-    style.configure("TButton", 
-                    background="#30833f",  
-                    foreground="white",    
+    style.configure("TButton",
+                    background="#30833f",
+                    foreground="white",
                     font=("Helvetica", 10, "bold"),
                     padding=5)
 
     ttk.Button(main_window, text="Adicionar Resíduo", command=insert_data).grid(row=3, column=0, columnspan=2, pady=5, padx=10, sticky="n")
 
-    if role == 'admin':  # Apenas engenheiro chefe (admin) pode remover
+    if role == 'admin':
         ttk.Button(main_window, text="Remover Resíduo", command=delete_data).grid(row=4, column=0, columnspan=2, pady=5, padx=10, sticky="n")
     
     ttk.Button(main_window, text="Visualizar Status", command=view_data).grid(row=5, column=0, columnspan=2, pady=5, padx=10, sticky="n")
@@ -185,7 +175,6 @@ def open_main_window(username, role):
 
     main_window.mainloop()
 
-# Interface de login
 root = tk.Tk()
 root.title("Login")
 root.geometry("400x300")
@@ -205,15 +194,14 @@ entry_senha.grid(row=1, column=1, pady=10, padx=20)
 
 style = ttk.Style()
 style.theme_use("clam")
-
-style.configure("TButton", 
-                background="#4CAF50", 
+style.configure("TButton",
+                background="#4CAF50",
                 foreground="white",
                 font=("Helvetica", 12, "bold"),
                 padding=10)
 
-style.map("TButton", 
-          background=[("active", "#45a049")], 
+style.map("TButton",
+          background=[("active", "#45a049")],
           foreground=[("active", "white")])
 
 btn_login = ttk.Button(root, text="Login", command=login)
